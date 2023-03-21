e35babe7d9396998964e5aee34bb957029de753f
tahirch0udhary <f201028@cfd.nu.edu.pk>
2023-03-22 04:59:34 +0500

    doctor crud operations

--- Index.js
+++ Index.js
@@ -1,10 +1,20 @@
-const express = require("express");
-const app = express();
-const port = 3300;
-const requireRoute = require("./Routes/routescont");
+const bodyParser = require('body-parser');
+const express = require('express')
+const app = express()
+const hostname = '127.0.0.1';
+const port = 3000;
 
-app.use("/", requireRoute)
+const userroute = require('./routes/router');
 
-app.listen(port, () => {
-    console.log("hi server running");
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use('/user', userroute)
+
+app.listen(port, hostname, () => {
+    console.log(`Running At http://${hostname}:${port}/`);
 })
+
+
